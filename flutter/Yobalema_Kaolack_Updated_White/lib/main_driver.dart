b2c4c7dc9697689eb0b634e940d229783e995573
Babacar Ndiaye <mbayendiayendoffane@gmail.com>
2026-08-28 21:10:02 +0000

    refactor: wire driver app to shared services

--- flutter/Yobalema_Kaolack_Updated_White/lib/main_driver.dart
+++ flutter/Yobalema_Kaolack_Updated_White/lib/main_driver.dart
@@ -1,3 +1,6 @@
+import 'package:flutter/material.dart';
+
+import 'core/app/app_services.dart';
 import 'driver/driver_features.dart';
 
-void main() => runApp(const DriverFeatureApp());
+void main() => runApp(DriverFeatureApp(services: AppServices()));
