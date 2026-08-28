986d8e9871472bd53db20ef305d031e4fb983820
Babacar Ndiaye <mbayendiayendoffane@gmail.com>
2026-08-28 20:36:06 +0000

    refactor(passenger): clean feature composition shell

--- flutter/Yobalema_Kaolack_Updated_White/lib/passenger/passenger_features.dart
+++ flutter/Yobalema_Kaolack_Updated_White/lib/passenger/passenger_features.dart
@@ -1,18 +1,40 @@
 import 'package:flutter/material.dart';
+
 import '../api.dart';
 import 'auth/passenger_auth_screen.dart';
 
 class PassengerFeatureApp extends StatefulWidget {
   const PassengerFeatureApp({super.key});
-  @override State<PassengerFeatureApp> createState() => _PassengerFeatureAppState();
+
+  @override
+  State<PassengerFeatureApp> createState() => _PassengerFeatureAppState();
 }
+
 class _PassengerFeatureAppState extends State<PassengerFeatureApp> {
-  final api = YobalemaApi();
-  @override void dispose() { api.dispose(); super.dispose(); }
-  @override Widget build(BuildContext context) => MaterialApp(
-    debugShowCheckedModeBanner: false,
-    title: 'Yobalema Passenger',
-    theme: ThemeData(useMaterial3: true, colorSchemeSeed: const Color(0xFFFFCC00)),
-    home: PassengerAuthScreen(api: api),
-  );
+  late final YobalemaApi _api;
+
+  @override
+  void initState() {
+    super.initState();
+    _api = YobalemaApi();
+  }
+
+  @override
+  void dispose() {
+    _api.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Yobalema Passenger',
+      theme: ThemeData(
+        useMaterial3: true,
+        colorSchemeSeed: const Color(0xFFFFCC00),
+      ),
+      home: PassengerAuthScreen(api: _api),
+    );
+  }
 }
