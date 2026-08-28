7d94f759e3581f4c473d51bed5a38c60f4037473
Babacar Ndiaye <mbayendiayendoffane@gmail.com>
2026-08-28 21:09:54 +0000

    refactor: wire passenger app to shared services

--- flutter/Yobalema_Kaolack_Updated_White/lib/main_passenger.dart
+++ flutter/Yobalema_Kaolack_Updated_White/lib/main_passenger.dart
@@ -1,3 +1,6 @@
+import 'package:flutter/material.dart';
+
+import 'core/app/app_services.dart';
 import 'passenger/passenger_features.dart';
 
-void main() => runApp(const PassengerFeatureApp());
+void main() => runApp(PassengerFeatureApp(services: AppServices()));
