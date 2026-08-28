05244c60dc5bca93747bdd9a2a6066f56a4a1f5c
Babacar Ndiaye <mbayendiayendoffane@gmail.com>
2026-08-28 21:14:58 +0000

    refactor: inject shared services into passenger shell

--- flutter/Yobalema_Kaolack_Updated_White/lib/passenger/passenger_features.dart
+++ flutter/Yobalema_Kaolack_Updated_White/lib/passenger/passenger_features.dart
@@ -1,30 +1,18 @@
 import 'package:flutter/material.dart';
 
-import '../core/network/api_client.dart';
+import '../core/app/app_services.dart';
 import 'auth/passenger_auth_screen_v2.dart';
 
 class PassengerFeatureApp extends StatefulWidget {
-  const PassengerFeatureApp({super.key});
+  const PassengerFeatureApp({super.key, required this.services});
+
+  final AppServices services;
 
   @override
   State<PassengerFeatureApp> createState() => _PassengerFeatureAppState();
 }
 
 class _PassengerFeatureAppState extends State<PassengerFeatureApp> {
-  late final ApiClient _api;
-
-  @override
-  void initState() {
-    super.initState();
-    _api = ApiClient();
-  }
-
-  @override
-  void dispose() {
-    _api.dispose();
-    super.dispose();
-  }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -34,7 +22,7 @@ class _PassengerFeatureAppState extends State<PassengerFeatureApp> {
         useMaterial3: true,
         colorSchemeSeed: const Color(0xFFFFCC00),
       ),
-      home: PassengerAuthScreenV2(api: _api),
+      home: PassengerAuthScreenV2(services: widget.services),
     );
   }
 }
