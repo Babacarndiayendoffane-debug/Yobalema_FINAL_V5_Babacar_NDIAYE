ee2b8961c9bbe3c900f64ad47886884bc07b7790
Babacar Ndiaye <mbayendiayendoffane@gmail.com>
2026-08-28 21:19:40 +0000

    fix: wire passenger shell to ApiClient contract

--- flutter/Yobalema_Kaolack_Updated_White/lib/passenger/passenger_features.dart
+++ flutter/Yobalema_Kaolack_Updated_White/lib/passenger/passenger_features.dart
@@ -3,16 +3,11 @@ import 'package:flutter/material.dart';
 import '../core/app/app_services.dart';
 import 'auth/passenger_auth_screen_v2.dart';
 
-class PassengerFeatureApp extends StatefulWidget {
+class PassengerFeatureApp extends StatelessWidget {
   const PassengerFeatureApp({super.key, required this.services});
 
   final AppServices services;
 
-  @override
-  State<PassengerFeatureApp> createState() => _PassengerFeatureAppState();
-}
-
-class _PassengerFeatureAppState extends State<PassengerFeatureApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -22,7 +17,7 @@ class _PassengerFeatureAppState extends State<PassengerFeatureApp> {
         useMaterial3: true,
         colorSchemeSeed: const Color(0xFFFFCC00),
       ),
-      home: PassengerAuthScreenV2(services: widget.services),
+      home: PassengerAuthScreenV2(api: services.apiClient),
     );
   }
 }
